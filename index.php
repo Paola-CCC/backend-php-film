9988100288c2c194723b9f8298e85995502e54b1
Paola_C <paola.cyprien@hetic.net>
2024-06-09 21:01:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD');
+header('Content-Type: application/json');
+
+
+require 'vendor/autoload.php';
+require 'src/Utils/config.php';
+
+use App\Routing\Router;
+$router = new Router();
+echo $router->run();
